42e416766baa4268fec1803fd0a00422d4d757c0
CyCle <cycle5328@naver.com>
2021-12-27 22:06:42 +0900

    back to the list when searching

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import style from "./video_item.module.css";
 
-const VideoItem = ({ video: { snippet } }) => (
-  <li className={style.container}>
-    <div className={style.video}>
-      <img
-        src={snippet.thumbnails.medium.url}
-        alt="videoThumbnail"
-        className={style.thumbnail}
-      />
-      <div className={style.metadata}>
-        <p className={style.title}>{snippet.title}</p>
-        <p className={style.channel}>{snippet.channelTitle}</p>
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === "list" ? style.list : style.grid;
+  return (
+    <li
+      className={`${style.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={style.video}>
+        <img
+          src={snippet.thumbnails.medium.url}
+          alt="videoThumbnail"
+          className={style.thumbnail}
+        />
+        <div className={style.metadata}>
+          <p className={style.title}>{snippet.title}</p>
+          <p className={style.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default VideoItem;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import VideoItem from "../video_item/video_item";
 import style from "./video_list.module.css";
 
-const VideoList = (props) => (
+const VideoList = ({ videos, onVideoClick, display }) => (
   <ul className={style.videos}>
-    {props.videos.map((video) => {
-      return <VideoItem key={video.id} video={video} />;
+    {videos.map((video) => {
+      return (
+        <VideoItem
+          key={video.id}
+          video={video}
+          onVideoClick={onVideoClick}
+          display={display}
+        />
+      );
     })}
   </ul>
 );
